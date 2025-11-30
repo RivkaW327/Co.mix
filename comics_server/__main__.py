eae5d8f7604fe51e3150f9687cf48a8d8fa4e11a
RivkaW327 <rw0527659123@gmail.com>
2025-11-30 10:48:54 +0200

    Remove ignored files from git tracking

--- comics_server/__main__.py
+++
@@ -1,8 +0,0 @@
-import uvicorn
-from .main import app
-from .config.config_loader import config
-import logging
-logging.basicConfig(level=logging.INFO)
-
-if __name__ == "__main__":
-    uvicorn.run("FastAPIProject.main:app", host=config["server"]["host"], port=config["server"]["port"])#, reload=True)
